d0d5f27be7e1c6428d95a5dbe52a902657dbec49
Brian Ketelsen <bketelsen@gmail.com>
2022-12-25 20:51:59 -0500

    more personalized

--- home/zsh.nix
+++ home/zsh.nix
@@ -5,6 +5,7 @@
 {
   enable = true;
   enableAutosuggestions = true;
+  enableCompletion = true;
   autocd = false;
   initExtra = (builtins.readFile ./scripts/init.sh);
   shellAliases = (import ./aliases.nix { inherit homeDirectory; }).shell;
